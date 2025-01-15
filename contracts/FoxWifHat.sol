9190be2b85fc69142c46c2ce50f499c67d79abbb
0xean <0xean.eth@gmail.com>
2025-01-15 13:21:09 -0400

    add basic strucuture for merkle framework

--- contracts/FoxWifHat.sol
+++ contracts/FoxWifHat.sol
@@ -1,6 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.28;
-
+pragma solidity =0.8.17;
 
 import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
@@ -10,17 +9,22 @@ import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 /// @dev ERC20 Meme token 
 contract FoxWifHat is AccessControl, ERC20("FOX Wif Hat", "FWH") {
     
+    /// @dev The identifier of the role which maintains other roles.
+    bytes32 public constant ADMIN_ROLE = keccak256("ADMIN");
+
     /// @dev The identifier of the role which allows accounts to mint tokens.
     bytes32 public constant MINTER_ROLE = keccak256("MINTER");
-
+   
     constructor() {
-        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
-        _grantRole(MINTER_ROLE, msg.sender);
+        _setupRole(ADMIN_ROLE, msg.sender);
+        _setupRole(MINTER_ROLE, msg.sender);
+        _setRoleAdmin(MINTER_ROLE, ADMIN_ROLE);
+        _setRoleAdmin(ADMIN_ROLE, ADMIN_ROLE);
     }
 
     /// @dev A modifier which checks that the caller has the minter role.
     modifier onlyMinter() {
-        require(hasRole(MINTER_ROLE, msg.sender), "FWH: only minter");
+        require(hasRole(MINTER_ROLE, msg.sender), "TicToken: only minter");
         _;
     }
 
---
+++ scripts/deploy.js
@@ -0,0 +1,50 @@
+const { ethers } = require('hardhat')
+
+const DAO_MSIG_ADDRESS = ""
+const MERKLE_ROOT = ""
+const MINT_AMOUNT = ""
+
+
+async function main() {
+  accounts = await ethers.getSigners();
+
+  // deploy Fox Wif Hat
+  const FoxWifHat = await ethers.getContractFactory('FoxWifHat')
+  const foxWifHat = await FoxWifHat.deploy()
+  await foxWifHat.deployed()
+
+  // assign admin and mint to DAO msig
+  const adminRole = await foxWifHat.ADMIN_ROLE();
+  const minterRole = await foxWifHat.MINTER_ROLE();
+  
+  console.log(`Granting Admin to DAO: ${DAO_MSIG_ADDRESS}`);
+  await ticToken.grantRole(adminRole, DAO_MSIG_ADDRESS);
+  
+  console.log(`Granting Minter to DAO: ${DAO_MSIG_ADDRESS}`);
+  await ticToken.grantRole(minterRole, DAO_MSIG_ADDRESS);
+
+  // deploy MerkleDistributor
+  const MerkleDistributor = await ethers.getContractFactory('MerkleDistributor')
+  const merkleDistributor = await MerkleDistributor.deploy(foxWifHat.address, MERKLE_ROOT)
+  await merkleDistributor.deployed()
+  
+  console.log(`MerkleDistributor deployed at ${merkleDistributor.address}`)
+
+  // mint FOX wif hat to merkle distributor
+  await foxWifHat.mint(merkleDistributor.address, MINT_AMOUNT)
+
+  // remove our admin and minting permissions
+  console.log(`Renouncing Admin Role`);
+  await ticToken.renounceRole(adminRole, accounts[0].address);
+  console.log(`Renouncing Minter Role`);
+  await ticToken.renounceRole(minterRole, accounts[0].address);
+}
+
+main()
+  // eslint-disable-next-line no-process-exit
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error)
+    // eslint-disable-next-line no-process-exit
+    process.exit(1)
+  })
